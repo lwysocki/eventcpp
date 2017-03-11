88f5f6d15e1d7d03e5a4e0ce1e38d4a855d74815
Lukasz Wysocki <wysocki.l@gmail.com>
2017-03-11 20:05:27 +0100

    Added methods for object pointer and inline operator !=

--- event.h
+++ event.h
@@ -12,94 +12,91 @@
 #include <list>
 #include <type_traits>
 
+template<typename TRet, typename ...Args> class InvokableAbstract;
+
+template<typename TRet, typename ...Args>
+inline bool operator!= (const InvokableAbstract<TRet, Args...>& lhs,
+    const InvokableAbstract<TRet, Args...>& rhs)
+{
+    return lhs.GetFuncPtr() != rhs.GetFuncPtr() && lhs.GetObjPtr() != rhs.GetObjPtr();
+}
+
 template<typename TRet, typename ...Args>
 class InvokableAbstract
 {
 public:
-	virtual TRet operator() (Args&&...) = 0;
-	virtual uintptr_t GetFuncPtr() const = 0;
-	virtual uintptr_t GetObjPtr() const = 0;
-
-	bool operator== (const InvokableAbstract<TRet, Args...>& rhs)
-	{
-		bool value = GetFuncPtr() == rhs.GetFuncPtr() && GetObjPtr() == rhs.GetObjPtr();
-		return value;
-	}
-
-	bool operator!= (const InvokableAbstract<TRet, Args...>& rhs)
-	{
-		bool value = !operator==(rhs);
-		return value;
-	}
+    virtual TRet operator() (Args&&...) = 0;
+    virtual uintptr_t GetFuncPtr() const = 0;
+    virtual uintptr_t GetObjPtr() const = 0;
 };
 
 template<typename TRet, typename ...Args>
-class InvokableFunc: public InvokableAbstract<TRet, Args...>
+class InvokableFunc : public InvokableAbstract<TRet, Args...>
 {
-	using _TFuncPtr = typename std::add_pointer<TRet(Args...)>::type;
+    using _TFuncPtr = typename std::add_pointer<TRet(Args...)>::type;
 
 public:
-	InvokableFunc(_TFuncPtr func): _func(func)
-	{
-	}
+    InvokableFunc(_TFuncPtr func) : _func(func)
+    {
+    }
 
-	virtual TRet operator() (Args&&... args)
-	{
-		if(_func == nullptr)
-			throw std::bad_function_call();
+    virtual TRet operator() (Args&&... args)
+    {
+        if (_func == nullptr)
+            throw std::bad_function_call();
 
-		return std::invoke(_func, std::forward<Args>(args)...);
-	}
+        return std::invoke(_func, std::forward<Args>(args)...);
+    }
 
-	virtual uintptr_t GetFuncPtr() const
-	{
-		return reinterpret_cast<uintptr_t>(_func);
-	}
+    virtual uintptr_t GetFuncPtr() const
+    {
+        return reinterpret_cast<uintptr_t>(_func);
+    }
 
-	virtual uintptr_t GetObjPtr() const
-	{
-		return reinterpret_cast<uintptr_t>(nullptr);
-	}
+    virtual uintptr_t GetObjPtr() const
+    {
+        return reinterpret_cast<uintptr_t>(nullptr);
+    }
 
 private:
-	_TFuncPtr _func;
+    _TFuncPtr _func;
 };
 
 template<typename TRet, typename TClass, typename ...Args>
-class InvokableMember: public InvokableAbstract<TRet, Args...>
+class InvokableMember : public InvokableAbstract<TRet, Args...>
 {
-	using _TFuncPtr = TRet(TClass::*) (Args...);
-	using _TClassRef = typename std::add_lvalue_reference<TClass>::type;
+    using _TFuncPtr = TRet(TClass::*) (Args...);
+    using _TClassRef = typename std::add_lvalue_reference<TClass>::type;
 
 public:
-	InvokableMember(_TFuncPtr func, _TClassRef obj): _func(func), _obj(obj)
-	{
-	}
+    InvokableMember(_TFuncPtr func, _TClassRef obj) : _func(func), _obj(obj)
+    {
+    }
 
-	virtual TRet operator() (Args&&... args)
-	{
-		if(_func == nullptr || &_obj == nullptr)
-			throw std::bad_function_call();
+    virtual TRet operator() (Args&&... args)
+    {
+        if (_func == nullptr || &_obj == nullptr)
+            throw std::bad_function_call();
 
-		return std::invoke(_func, _obj, std::forward<Args>(args)...);
-	}
+        return std::invoke(_func, _obj, std::forward<Args>(args)...);
+    }
 
-	virtual uintptr_t GetFuncPtr() const
-	{
-		uintptr_t converted;
-		std::memcpy(&converted, &_func, sizeof(_func));
+    virtual uintptr_t GetFuncPtr() const
+    {
+        uintptr_t converted;
+        std::memcpy(&converted, &_func, sizeof(_func));
 
-		return converted;
-	}
+        return converted;
+    }
 
-	virtual uintptr_t GetObjPtr() const
-	{
-		return reinterpret_cast<uintptr_t>(&_obj);
-	}
+    virtual uintptr_t GetObjPtr() const
+    {
+        return reinterpret_cast<uintptr_t>(&_obj);
+    }
 
 private:
-	_TFuncPtr _func;
-	_TClassRef _obj;
+    _TFuncPtr _func;
+    _TClassRef _obj;
 };
 
 template<typename TFunc> class Event;
@@ -140,6 +137,12 @@ public:
 		_invokables.push_back(ptr);
 	}
 
+    template<typename TClass>
+    void Attach(TRet(TClass::* func) (Args...), TClass* obj)
+    {
+        Attach(func, *obj);
+    }
+
 	template<typename TBase, typename TClass>
 	void Attach(TRet(TBase::* func) (Args...), TClass& obj)
 	{
@@ -147,6 +150,12 @@ public:
 		ptr.reset(new InvokableMember<TRet, TClass, Args...>(func, obj));
 		_invokables.push_back(ptr);
 	}
+
+    template<typename TBase, typename TClass>
+    void Attach(TRet(TBase::* func) (Args...), TClass* obj)
+    {
+        Attach(func, *obj);
+    }
 
 	void Detach(_TFuncPtr func)
 	{
@@ -161,12 +170,24 @@ public:
 		Remove(invokable);
 	}
 
+    template<typename TClass>
+    void Detach(_TFunc TClass::* func, TClass* obj)
+    {
+        Detach(func, *obj);
+    }
+
 	template<typename TBase, typename TClass>
 	void Detach(_TFunc TBase::* func, TClass& obj)
 	{
 		InvokableMember<TRet, TClass, Args...> invokable(func, obj);
 		Remove(invokable);
 	}
+
+    template<typename TBase, typename TClass>
+    void Detach(_TFunc TBase::* func, TClass* obj)
+    {
+        Detach(func, *obj);
+    }
 
 private:
 	std::list<std::shared_ptr<_TInvokable>> _invokables;
